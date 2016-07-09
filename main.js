a6152829a890d930b9655255ab5204964dae972f
nickangtc <nickangtc@gmail.com>
2016-07-09 16:28:06 +0800

    modified js to include comparisons

--- main.js
+++ main.js
@@ -3,3 +3,9 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
+if (cardTwo === cardFour) {
+	alert ("You found a match!");
+}
+else if (cardTwo !== cardFour) {
+	alert ("Sorry, try again.");
+}
