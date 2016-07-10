cab07fa238d5840c254b1e9f6306f8d29bb6c5ab
nickangtc <nickangtc@gmail.com>
2016-07-10 19:44:37 +0800

    added createBoard function

--- main.js
+++ main.js
@@ -3,9 +3,21 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-if (cardTwo === cardFour) {
-	alert ("You found a match!");
+// if (cardTwo === cardFour) {
+// 	alert ("You found a match!");
+// }
+// else if (cardTwo !== cardFour) {
+// 	alert ("Sorry, try again.");
+// }
+
+var gameBoard = document.getElementById('game-board');
+var createBoard = function() {
+	for (var j = 0; j < 4; j++) {
+		var newCard = document.createElement('div');
+		newCard.className = 'card';
+		gameBoard.appendChild(newCard);
+	}
+	console.log(document.getElementById('game-board').value);
 }
-else if (cardTwo !== cardFour) {
-	alert ("Sorry, try again.");
-}
+
+createBoard();
